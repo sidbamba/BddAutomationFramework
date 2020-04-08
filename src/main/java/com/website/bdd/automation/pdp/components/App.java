9ac709cca241a59776ff760829560b42ceee8035
Sidharth Bamba <sidbamba@publicisgroupe.net>
2020-04-08 15:28:52 +0530

    folder structure

---
+++ src/main/java/com/website/bdd/automation/pdp/components/App.java
@@ -0,0 +1,13 @@
+package com.website.bdd.automation.pdp.components;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/bdd/automation/framework/it/AppTest.java
@@ -0,0 +1,21 @@
+package com.bdd.automation.framework.it;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/html/Report.html" ,"json:target/html/cucumber.json"}, features = {""},
+glue = {""}, tags = {""}, monochrome= true)
+	
+public class AppTest {
+	
+}
